3f89eb5a646877a90a11c3f20bece6b605ef6382
Akanimorex <akanimoime2016@gmail.com>
2024-10-03 13:14:43 +0100

    added testnet chain

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,11 +20,31 @@ import {
 
 import HelloWorld from './HelloWorld';
 
+const coreTestnet = {
+  id: 1115,
+  name: 'Core Testnet',
+  iconUrl: 'https://images.app.goo.gl/rqMHLjxM8YPaGZHT9',
+  iconBackground: '#fff',
+  nativeCurrency: { name: 'CORE', symbol: 'tCORE', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['https://rpc.test.btcs.network'] },
+  },
+  blockExplorers: {
+    default: { name: 'Core Explorer', url: 'https://scan.test.btcs.network/' },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 11_907_934,
+    },
+  },
+};
+
 
 const config = getDefaultConfig({
   appName: 'Core DAO Hello World',
   projectId: '2c22698ed6fa65b5ab4a6acb4af0b952',
-  chains: [coreDao],
+  chains: [coreDao,coreTestnet],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
